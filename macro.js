9df1d476a8eb059f112bcaf4aa560cc1fdfde3fe
Cadu3011 <cadulinker132@gmail.com>
2025-02-26 17:04:41 -0300

    lançando projeto ao git

---
+++ macro.js
@@ -0,0 +1,83 @@
+const { chromium } = require('playwright');
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = 8000;
+const path = require('path');
+// Configurar o servidor express
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+// Endpoint para rodar o relatório
+app.post('/api/rodar-relatorio', async (req, res) => {
+    const { dataInicio, dataFim } = req.body;
+    
+    // Validação das datas
+    if (!dataInicio || !dataFim) {
+        return res.status(400).json({ message: 'Datas inválidas' });
+    }
+
+    let value = '';
+    try {
+        // Lançar o navegador com Playwright
+        const browser = await chromium.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        });
+
+        const page = await browser.newPage();
+        await page.route('**/*', (route, request) => {
+            if (['image', 'stylesheet', 'font'].includes(request.resourceType())) {
+                route.abort(); // Aborta o carregamento de imagens, fontes e folhas de estilo
+            } else {
+                route.continue();  // Deixa as outras requisições passarem
+            }
+        });
+        await page.goto('http://192.168.1.253:4647/sgfpod1/Login.pod', { waitUntil: 'domcontentloaded' });
+
+        // Login e navegação
+        await page.fill('#id_cod_usuario', '95');
+        await page.fill('#nom_senha', 'cadu3011');
+        await page.click('#login');
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+        await page.goto('http://192.168.1.253:4647/sgfpod1/Rel_0015.pod?cacheId=1733840049536', {
+            waitUntil: 'domcontentloaded',
+        });
+        console.log("foi pg rel")
+        await page.click('#tabTabdhtmlgoodies_tabView1_3 > a');
+        await page.click('#selecao2_1');
+        await page.click('#tabTabdhtmlgoodies_tabView1_4 > a');
+        await page.fill('#dat_inicio', dataInicio);
+        await page.fill('#dat_fim', dataFim);
+        await page.waitForSelector('#saida_3');
+        await page.click('#saida_3');
+
+        const context = browser.contexts()[0];
+        const [newPage] = await Promise.all([
+            context.waitForEvent('page', { timeout: 10000 }),
+            page.click('#runReport')
+        ]);
+        console.log("gerou rel")
+        try {
+            await newPage.waitForSelector('body > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(30) > td:nth-child(9) > span', { timeout: 2000 });
+
+             value = await newPage.$eval('body > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(30) > td:nth-child(9) > span', (element) => element.innerText);
+            await browser.close();
+            res.json({ totalCompra: value });
+        } catch (error) {
+            console.log('Seletor não encontrado dentro do tempo limite de 2 segundos. Fechando a aba.');
+        }
+    } catch (error) {
+        console.log('Erro: A nova aba não foi aberta dentro do tempo limite de 50 segundos. Fechando o navegador.');
+        res.status(500).json({ message: 'Erro ao rodar o script Playwright' }); 
+    }
+       
+});
+
+app.get('/buscar-total-perfumeria',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'compras.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
---
+++ public/app.js
@@ -0,0 +1,79 @@
+let result
+
+document.getElementById("buscarBtn").addEventListener("click", async function() {
+    const dataInicio = document.getElementById("dataInicio").value;
+    const dataFim = document.getElementById("dataFim").value;
+    let formattedDataInicio = ''
+    let formattedDataFim = ''
+    if (!dataInicio || !dataFim) {
+        alert("Por favor, selecione ambas as datas.");
+        return;
+    }
+
+    const resultado = document.getElementById("resultado");
+    const loadingIcon = document.getElementById("loading");
+
+    // Exibir o ícone de carregamento
+    loadingIcon.style.display = "inline-block";
+
+    // Função para formatar a data no formato DD/MM/YYYY
+    function formatDate(date) {
+        const [year, month, day] = date.split('-'); // Separando a data no formato YYYY-MM-DD
+        return `${day.padStart(2, '0')}/${month.padStart(2, '0')}/${year}`;
+    }
+
+    // Validação de data de início antes de data de fim
+    if (new Date(dataInicio) > new Date(dataFim)) {
+        resultado.textContent = "A data de início não pode ser maior que a data de fim.";
+        resultado.style.color = "red";
+        loadingIcon.style.display = "none"; // Esconde o ícone de carregamento
+    } else {
+        formattedDataInicio = formatDate(dataInicio);
+        formattedDataFim = formatDate(dataFim);
+        resultado.textContent = `Período selecionado: ${formattedDataInicio} até ${formattedDataFim}`;
+        resultado.style.color = "#3498db";
+    }
+
+    // Chama a API ou endpoint que rodará o script Playwright no backend
+    try {
+        const response = await fetch('http://localhost:8000/api/rodar-relatorio', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                dataInicio: formattedDataInicio,  // Envia como 'YYYY-MM-DD', pois o backend pode precisar disso
+                dataFim: formattedDataFim         // Envia como 'YYYY-MM-DD'
+            })
+        });
+        
+         result = await response.json();
+        const totalCompra = result.totalCompra || 'Valor não encontrado';
+        document.getElementById("totalCompra").textContent = `Valor total de compras de perfumaria: ${totalCompra}`;
+    } catch (error) {
+        console.error("Erro ao chamar a API:", error);
+        document.getElementById("totalCompra").textContent = 'Erro ao obter o valor.';
+    } finally {
+        // Esconde o ícone de carregamento, independentemente de sucesso ou falha
+        loadingIcon.style.display = "none";
+    }
+});
+
+document.getElementById("calcularBtn").addEventListener("click", async function() {
+    const orcamento = document.getElementById("orcamento").value;
+    const totalCompra = result.totalCompra;
+
+    // Remover a vírgula e substituir por ponto
+    const totalCompraFormatado = totalCompra.replace('.', '').replace(',', '.');  // Troca a vírgula por ponto
+    const orcamentoFormatado = orcamento.replace(',', '.');  // Caso o orçamento também tenha vírgula
+    
+    // Converter para números
+    const valorOrcamento = parseFloat(orcamentoFormatado);
+    const valorTotalCompra = parseFloat(totalCompraFormatado);
+
+    // Cálculo
+    const calculo = valorOrcamento - valorTotalCompra;
+    const calculoFormatado = calculo.toLocaleString('pt-BR', { style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    // Exibir o valor disponível
+    document.getElementById("orcamentoDisponivel").textContent = `Orçamento disponível: R$ ${calculoFormatado}`;
+});
